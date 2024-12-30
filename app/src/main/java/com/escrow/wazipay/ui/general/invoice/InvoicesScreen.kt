be553ed34a863e868046696efb63b3c7674e9d6f
Mbogo-anonymous <mbogoalex3@gmail.com>
2024-12-30 10:00:49 +0300

    invoices screen

---
+++ app/src/main/java/com/escrow/wazipay/ui/general/invoice/InvoicesScreen.kt
@@ -0,0 +1,73 @@
+package com.escrow.wazipay.ui.general.invoice
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.escrow.wazipay.data.network.models.invoice.InvoiceData
+import com.escrow.wazipay.data.network.models.invoice.invoices
+import com.escrow.wazipay.ui.theme.WazipayTheme
+import com.escrow.wazipay.utils.screenFontSize
+import com.escrow.wazipay.utils.screenHeight
+import com.escrow.wazipay.utils.screenWidth
+
+@Composable
+fun InvoicesScreenComposable(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun InvoicesScreen(
+    profile: String,
+    invoices: List<InvoiceData>,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                horizontal = screenWidth(x = 16.0),
+                vertical = screenHeight(x = 16.0)
+            )
+    ) {
+        Text(
+            text = if(profile == "Buyer") "My Invoices" else "Received Invoices",
+            fontWeight = FontWeight.Bold,
+            fontSize = screenFontSize(x = 14.0).sp
+        )
+        Spacer(modifier = Modifier.height(screenHeight(x = 16.0)))
+        LazyColumn {
+            items(invoices) {
+                InvoiceItemComposable(
+                    invoiceData = it
+                )
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun InvoicesScreenPreview() {
+    WazipayTheme {
+        InvoicesScreen(
+            profile = "Buyer",
+            invoices = invoices
+        )
+    }
+}
